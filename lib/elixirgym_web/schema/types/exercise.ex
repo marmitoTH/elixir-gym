39327d53da45ec00a02f4fa6bd4f2620004d83bf
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2021-08-17 12:45:22 -0300

    Create Training and Exercise GraphQL schema types

---
+++ lib/elixirgym_web/schema/types/exercise.ex
@@ -0,0 +1,19 @@
+defmodule ElixirgymWeb.Schema.Types.Exercise do
+  use Absinthe.Schema.Notation
+
+  @desc "Logic exercise representation"
+  object :exercise do
+    field :id, non_null(:uuid4)
+    field :name, non_null(:string)
+    field :description, non_null(:string)
+    field :repetitions, non_null(:string)
+    field :video_url, non_null(:string)
+  end
+
+  input_object :create_exercise_input do
+    field :name, non_null(:string)
+    field :description, non_null(:string)
+    field :repetitions, non_null(:string)
+    field :video_url, non_null(:string)
+  end
+end
