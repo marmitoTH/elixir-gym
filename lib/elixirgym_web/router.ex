fa59df5ec237a0f6e7634d450822a6a9a21b087b
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2021-08-17 10:20:58 -0300

    Add GraphQL routes to the API route

--- lib/elixirgym_web/router.ex
+++ lib/elixirgym_web/router.ex
@@ -11,6 +11,13 @@ defmodule ElixirgymWeb.Router do
     get "/imc", IMCController, :index
   end
 
+  scope "/api" do
+    pipe_through :api
+
+    forward "/graphql", Absinthe.Plug, schema: ElixirgymWeb.Schema
+    forward "/graphiql", Absinthe.Plug.GraphiQL, schema: ElixirgymWeb.Schema
+  end
+
   # Enables LiveDashboard only for development
   #
   # If you want to use the LiveDashboard in production, you should put
