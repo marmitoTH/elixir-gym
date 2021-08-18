3a30a44dea2a0798bf1e01f47f8b44709347c08d
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2021-08-18 11:53:00 -0300

    Return the most recent training on getUser query

--- lib/elixirgym/users/get.ex
+++ lib/elixirgym/users/get.ex
@@ -1,6 +1,8 @@
 defmodule Elixirgym.User.Get do
+  import Ecto.Query
+
   alias Ecto.UUID
-  alias Elixirgym.{Repo, User}
+  alias Elixirgym.{Repo, User, Training}
 
   def call(id) do
     id
@@ -15,7 +17,16 @@ defmodule Elixirgym.User.Get do
   defp handle_response({:ok, uuid}) do
     case Repo.get(User, uuid) do
       nil -> {:error, "User not found"}
-      user -> {:ok, user}
+      user -> {:ok, load_training(user)}
     end
+  end
+
+  defp load_training(user) do
+    today = Date.utc_today()
+    query =
+      from t in Training,
+        where: ^today >= t.start_date and ^today <= t.end_date
+
+    Repo.preload(user, trainings: {first(query, :inserted_at), :exercises})
   end
 end
