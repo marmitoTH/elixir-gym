47b5342c6e6291e0f5ffa7a25e03347bc0d30eff
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2021-08-18 15:07:55 -0300

    Configure test database

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # Run `mix help test` for more information.
 config :elixirgym, Elixirgym.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "docker",
   database: "elixirgym_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
